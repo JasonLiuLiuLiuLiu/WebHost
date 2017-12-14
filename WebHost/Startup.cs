080ac028546538a584a631f7cb8b8e1ec0ba6f66
刘振宇 <liuzhenyulive@live.com>
2017-12-14 21:50:12 +0800

    IHostingEnvironment env,IApplicationLifetime applicationLifetime的用法

--- WebHost/Startup.cs
+++ WebHost/Startup.cs
@@ -19,17 +19,33 @@ namespace WebHost
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env,IConfiguration configuration)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env,IConfiguration configuration,IApplicationLifetime applicationLifetime)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
-
+            applicationLifetime.ApplicationStarted.Register(() =>
+            {
+                Console.WriteLine("Strated");
+            });
+            applicationLifetime.ApplicationStopping.Register(() =>
+            {
+                Console.WriteLine("Stoping");
+            });
+            applicationLifetime.ApplicationStopped.Register(() =>
+            {
+                Console.WriteLine("Strated");
+            });
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync($"connectionString=\"{configuration["connectionString:defaultConnectionString"]}\"");
-                await context.Response.WriteAsync($"name=\"{configuration["name"]}\"");
+                await context.Response.WriteAsync($"name=\"{env.ApplicationName}\"");
+                await context.Response.WriteAsync($"name=\"{env.ContentRootFileProvider}\"");
+                await context.Response.WriteAsync($"name=\"{env.ContentRootPath}\"");
+                await context.Response.WriteAsync($"name=\"{env.EnvironmentName}\"");
+                await context.Response.WriteAsync($"name=\"{env.WebRootFileProvider}\"");   //不一一输出了  原理一样的
+                //await context.Response.WriteAsync($"connectionString=\"{configuration["connectionString:defaultConnectionString"]}\"");
+                //await context.Response.WriteAsync($"name=\"{configuration["name"]}\"");
                 await context.Response.WriteAsync("Hello World!");
             });
         }
